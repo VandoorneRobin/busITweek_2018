c5c0131ca9d1c370bb75cde54ab7d85b06fc0f82
Robin Vandoorne <robin.vandoorne@student.howest.be>
2018-02-08 09:34:28 +0100

    setting java main class

--- treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
+++ treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
@@ -1,13 +1,35 @@
-package busitweek18.treasurehunt.treasurehunt;
+package com.
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import cz.mendelu.busItWeek.library.StoryLine;
+import cz.mendelu.busItWeek.library.Task;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="MainActivity";
+
+    private StoryLine storyLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // SET HERE SCREEN TO LAUNCH ON START
         setContentView(R.layout.activity_main);
+
+        storyLine = StoryLine.open(this,MyDemoStoryLineDBHelper.class);
     }
+
+
+   
 }
