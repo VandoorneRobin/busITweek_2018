6eb1522216a13e82e0e0fb376c843e4ba751cb31
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2018-02-08 14:58:51 +0100

    Add and instantiate neccesary fields and objects (onCreate method) for MapsActivity

--- treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MapsActivity.java
+++ treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MapsActivity.java
@@ -2,17 +2,38 @@ package busitweek18.treasurehunt.treasurehunt;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+
+import cz.mendelu.busItWeek.library.StoryLine;
+import cz.mendelu.busItWeek.library.Task;
+import cz.mendelu.busItWeek.library.beacons.BeaconUtil;
+import cz.mendelu.busItWeek.library.map.MapUtil;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private StoryLine storyLine;
+    private Task task;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private BeaconUtil beaconUtil;
+    private HashMap<Task, Marker> markers = new HashMap<>();
+    private LatLngBounds.Builder latLngBoundsBuilder;
 
 
     @Override
@@ -23,6 +44,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        storyLine = StoryLine.open(this, TreasureHuntStoryLineDbHelper.class);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        googleApiClient.connect();
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+
+        beaconUtil = new BeaconUtil(this);
     }
 
 
@@ -40,6 +75,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+
+        // setting markers
+        MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(49.20997, 16.61479)).title("Treasure").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_delete_cross));
+        Marker marker = mMap.addMarker(markerOptions);
+
+
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
