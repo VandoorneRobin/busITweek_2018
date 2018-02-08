e83a1b3d5d19b63c5d779e314738316154d0e173
VandoorneRobin <robin.vandoorne@student.howest.be>
2018-02-08 12:48:52 +0100

    updated files

--- treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
+++ treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
@@ -1,18 +1,16 @@
-package bustitweek2017.treasurehunt.treasurehunt;
+package busitweek18.treasurehunt.treasurehunt;
 
         import android.content.Intent;
         import android.os.Bundle;
-
         import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
+        import android.view.View;
 
 
-        import cz.mendelu.busItWeek.library.StoryLine;
-        import cz.mendelu.busItWeek.library.Task;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG ="MainActivity";
 
-    private StoryLine storyLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +19,20 @@ public class MainActivity extends AppCompatActivity {
         // SET HERE SCREEN TO LAUNCH ON START
         setContentView(R.layout.activity_main);
 
-        storyLine = StoryLine.open(this,MyDemoStoryLineDBHelper.class);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    public void goToTreasures(View view) {
+        Log.i(TAG,"User clicked the start button");
+        Intent intent = new Intent(this, TreasurepickActivity.class);
+        startActivity(intent);
     }
 
 
