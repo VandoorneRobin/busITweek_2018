3eaefb3a37c22992164bb380c31ad685eb2ca4c1
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2018-02-08 13:49:17 +0100

    Start maps activity directly for development purposes

--- treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
+++ treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
@@ -1,5 +1,6 @@
 package busitweek18.treasurehunt.treasurehunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* for debug purposes */
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
