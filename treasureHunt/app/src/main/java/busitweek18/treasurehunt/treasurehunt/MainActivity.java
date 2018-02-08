7c1d25abc780ce4d2036560aed2768811da466e1
Robin Vandoorne <robin.vandoorne@student.howest.be>
2018-02-08 09:36:53 +0100

    updateing mainclass

--- treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
+++ treasureHunt/app/src/main/java/busitweek18/treasurehunt/treasurehunt/MainActivity.java
@@ -1,19 +1,13 @@
-package com.
+package bustitweek2017.treasurehunt.treasurehunt;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+        import android.content.Intent;
+        import android.os.Bundle;
 
-import cz.mendelu.busItWeek.library.StoryLine;
-import cz.mendelu.busItWeek.library.Task;
+        import android.support.v7.app.AppCompatActivity;
+
+
+        import cz.mendelu.busItWeek.library.StoryLine;
+        import cz.mendelu.busItWeek.library.Task;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG ="MainActivity";
@@ -23,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // SET HERE SCREEN TO LAUNCH ON START
         setContentView(R.layout.activity_main);
 
@@ -31,5 +25,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-   
+
 }
